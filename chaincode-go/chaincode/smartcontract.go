40c3aa75500b061eb38431199f193c5b484bd9eb
gus50cs <kostakis1999@hotmail.com>
2023-06-20 15:03:12 +0300

    Changes and fixes to the chaincoed

--- chaincode-go/chaincode/smartcontract.go
+++ chaincode-go/chaincode/smartcontract.go
@@ -5,11 +5,12 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"github.com/skip2/go-qrcode"
-	"reflect"
+	//"github.com/skip2/go-qrcode"
+	//"reflect"
 	//"reflect"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 	"time"
+	"strings"
 )
 
 // SmartContract provides functions for managing an Asset
@@ -20,69 +21,70 @@ type SmartContract struct {
 // Asset describes basic details of what makes up a simple asset
 // Insert struct field in alphabetic order => to achieve determinism across languages
 // golang keeps the order when marshal to json but doesn't order automatically
-type Asset struct {
-	AppraisedValue 	int               	`json:"AppraisedValue"`
-	Color          	string            	`json:"Color"`
-	ID             	string            	`json:"ID"`
-	Owner          	string            	`json:"Owner"`
-	Size           	int               	`json:"Size"`
-	QR             	string            	`json:"QR"`
-	GPS           	[]GPSCoordinate     `json:"GPS"`
-	Additional     	map[string]string 	`json:"Additional"`
+type LNGDocument struct {
+    DocumentID   string		`json:"DocumentID"`
+    DocumentCID  string		`json:"DocumentCID"`
+    Owner        string		`json:"Owner"`
+    AccessList   []string	`json:"AccessList"`
+	QR 			 string		`json:"QR"`
+	Timestamp 		 string		`json:"Timestamp"`
 }
 
-type GPSCoordinate struct {
-    Name        string  `json:"Name"`
-    Latitude    float64 `json:"Latitude"`
-    Longitude   float64 `json:"Longitude"`
-    Timestamp   string  `json:"Timestamp"`
+type AssetInfo struct {
+	DocumentID string `json:"DocumentID"`
+	Owner      string `json:"Owner"`
+	DocumentCID  string		`json:"DocumentCID"`
+	Timestamp 		 string		`json:"Timestamp"`
+}
+
+type AssetInfoOwner struct {
+	DocumentID string `json:"DocumentID"`
+	Owner      string `json:"Owner"`
+	DocumentCID  string		`json:"DocumentCID"`
+	AccessList   []string	`json:"AccessList"`
+	Timestamp 		 string		`json:"Timestamp"`
 }
 
 // CreateAsset issues a new asset to the world state with given details.
-func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id string, color string, size int, owner string, appraisedValue int, category string, value string, latitude float64, longitude float64, namestamp string) error {
-	exists, err := s.AssetExists(ctx, id)
+func (s *SmartContract) CreateDocument(ctx contractapi.TransactionContextInterface, DocumentID string, DocumentCID string, Owner string, access string) error{
+	exists, err := s.AssetExists(ctx, DocumentID, Owner)
 	if err != nil {
 		return err
 	}
 	if exists {
-		return fmt.Errorf("the asset %s already exists", id)
+		return fmt.Errorf("the asset %s already exists", DocumentID)
 	}
 
-	asset := Asset{
-		ID:             id,
-		Color:          color,
-		Size:           size,
-		Owner:          owner,
-		AppraisedValue: appraisedValue,
+	asset := LNGDocument{
+		DocumentID:		DocumentID,
+		DocumentCID:    DocumentCID,
+		Owner:          Owner,
+		Timestamp:		time.Now().Format("15:04:05 02-01-2006"),
 	}
-	asset = add(asset, category, value)
+
+	asset = accesslist(asset, access)
 	asset = hash(asset)
-	asset = gps(asset, latitude, longitude, namestamp)
-	code(asset)
 	qr := asset.QR
-	if err != nil {
-		return err
-	}
+
 	assetJSON, err := json.Marshal(asset)
 	if err != nil {
 		return err
 	}
 
 	return ctx.GetStub().PutState(qr, assetJSON)
+
 }
 
-// UpdateAsset updates an existing asset in the world state with provided parameters.
-func (s *SmartContract) UpdateGPS(ctx contractapi.TransactionContextInterface, id string, latitude float64, longitude float64, namestamp string) error {
-	asset, err := s.ReturnAsset(ctx, id)
+func (s *SmartContract) UpdateAccess(ctx contractapi.TransactionContextInterface, access string, DocumentID string, Owner string) error {
+	asset, err := s.ReturnAsset(ctx, DocumentID, Owner)
 	if err != nil {
 		return err
 	}
-	// overwriting original asset with new asset
-	asset = gps(asset, latitude, longitude, namestamp)
-	if err != nil {
-		return err
-	}
-	assetJSON, err := json.Marshal(asset)
+
+	// Update only the access list field of the asset
+	updatedAsset := accesslist(asset, access)
+
+	assetJSON, err := json.Marshal(updatedAsset)
 	if err != nil {
 		return err
 	}
@@ -90,26 +92,24 @@ func (s *SmartContract) UpdateGPS(ctx contractapi.TransactionContextInterface, i
 	return ctx.GetStub().PutState(asset.QR, assetJSON)
 }
 
-// DeleteAsset deletes an given asset from the world state.
-func (s *SmartContract) DeleteAsset(ctx contractapi.TransactionContextInterface, id string) error {
-	qrcode, err := s.GetQR(ctx, id)
+func (s *SmartContract) DeleteAsset(ctx contractapi.TransactionContextInterface, DocumentID string, Owner string) error {
+	qrcode, err := s.GetQR(ctx, DocumentID, Owner)
 	if err != nil {
 		return fmt.Errorf("failed to read from world state: %v", err)
 	}
-	exists, err := s.AssetExists(ctx, id)
+	exists, err := s.AssetExists(ctx, DocumentID, Owner)
 	if err != nil {
 		return err
 	}
 	if !exists {
-		return fmt.Errorf("the asset %s does not exist", id)
+		return fmt.Errorf("the asset %s does not exist", DocumentID)
 	}
 
 	return ctx.GetStub().DelState(qrcode)
 }
 
-// AssetExists returns true when asset with given ID exists in world state
-func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
-	qrcode, err := s.GetQR(ctx, id)
+func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface, DocumentID string, Owner string) (bool, error) {
+	qrcode, err := s.GetQR(ctx, DocumentID, Owner)
 	if err != nil {
 		return false, fmt.Errorf("failed to read from world state: %v", err)
 	}
@@ -120,147 +120,157 @@ func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface,
 	
 	return assetJSON != nil, nil
 }
-func (s *SmartContract) ReturnAsset(ctx contractapi.TransactionContextInterface, id string) (Asset, error) {
-	qrcode, err := s.GetQR(ctx, id)
+
+func (s *SmartContract) ReturnAsset(ctx contractapi.TransactionContextInterface, DocumentID string, Owner string) (LNGDocument, error) {
+	qrcode, err := s.GetQR(ctx, DocumentID, Owner)
 	if err != nil {
-		return Asset{}, fmt.Errorf("failed to read from world state: %v", err)
+		return LNGDocument{}, fmt.Errorf("failed to read from world state: %v", err)
 	}
 	assetJSON, err := ctx.GetStub().GetState(qrcode)
 	if err != nil {
-		return Asset{}, fmt.Errorf("failed to read from world state: %v", err)
+		return LNGDocument{}, fmt.Errorf("failed to read from world state: %v", err)
 	}
 	asset, err := DecodeAsset(assetJSON)
 	if err != nil {
-		return Asset{}, fmt.Errorf("failed to decode asset: %v", err)
+		return LNGDocument{}, fmt.Errorf("failed to decode asset: %v", err)
 	}
 	return asset, nil
 }
 
-// GetAllAssets returns all assets found in world state
-func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface, parameter string, value string) ([]*Asset, error) {
-	// range query with empty string for startKey and endKey does an
-	// open-ended query of all assets in the chaincode namespace.
+func (s *SmartContract) GetCIDByAccessName(ctx contractapi.TransactionContextInterface, accessName string) ([]AssetInfo, error) {
+	var filteredAssets []AssetInfo
+
 	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
 	if err != nil {
-		return nil, err
-	}
-	defer resultsIterator.Close()
-
-	var assets []*Asset
-	for resultsIterator.HasNext() {
-		queryResponse, err := resultsIterator.Next()
-		if err != nil {
-			return nil, err
-		}
-
-		var asset Asset
-		err = json.Unmarshal(queryResponse.Value, &asset)
-		if err != nil {
-			return nil, err
-		}
-
-		t := reflect.TypeOf(asset)
-		v := reflect.ValueOf(asset)
-
-		for i := 0; i < t.NumField(); i++ {
-			if parameter == t.Field(i).Name {
-				if fmt.Sprint(v.Field(i).Interface()) == value {
-					assets = append(assets, &asset)
-				}
-			}
-		}
-
-	}
-
-	return assets, nil
-}
-
-func (s *SmartContract) GetQR(ctx contractapi.TransactionContextInterface, value string) (string, error) {
-	// range query with empty string for startKey and endKey does an
-	// open-ended query of all assets in the chaincode namespace.
-	var assetQR string
-	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
-	if err != nil {
-		return assetQR, err
+		return filteredAssets, err
 	}
 	defer resultsIterator.Close()
 
 	for resultsIterator.HasNext() {
 		queryResponse, err := resultsIterator.Next()
 		if err != nil {
-			return assetQR, err
+			return filteredAssets, err
 		}
 
-		var asset Asset
+		var asset LNGDocument
 		err = json.Unmarshal(queryResponse.Value, &asset)
 		if err != nil {
-			return assetQR, err
+			return filteredAssets, err
 		}
 
-		t := reflect.TypeOf(asset)
-		v := reflect.ValueOf(asset)
-
-		for i := 0; i < t.NumField(); i++ {
-			fmt.Println(t.Field(i).Name)
-			if t.Field(i).Name == "ID" {
-				fmt.Println("mphke")
-				if fmt.Sprint(v.Field(i).Interface()) == value {
-					fmt.Sprint(v.Field(i).Interface())
-					QR := v.FieldByName("QR").Interface().(string)
-					assetQR = QR
-					fmt.Println(assetQR)
+		for _, name := range asset.AccessList {
+			if name == accessName {
+				assetInfo := AssetInfo{
+					DocumentID: asset.DocumentID,
+					DocumentCID: asset.DocumentCID,
+					Owner:      asset.Owner,
+					Timestamp: 		asset.Timestamp,
+					
 				}
+				filteredAssets = append(filteredAssets, assetInfo)
+				break
 			}
 		}
-
 	}
 
-	return assetQR, nil
+	return filteredAssets, nil
 }
 
-func (s *SmartContract) GPSsearch(ctx contractapi.TransactionContextInterface, name string) ([]*Asset, error) {
-	// range query with empty string for startKey and endKey does an
-	// open-ended query of all assets in the chaincode namespace.
+func (s *SmartContract) GetCIDByOwnerName(ctx contractapi.TransactionContextInterface, ownerName string) ([]AssetInfoOwner, error) {
+	var filteredAssets []AssetInfoOwner
+
 	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
 	if err != nil {
-		return nil, err
+		return filteredAssets, err
 	}
 	defer resultsIterator.Close()
 
-	var assets []*Asset
 	for resultsIterator.HasNext() {
 		queryResponse, err := resultsIterator.Next()
 		if err != nil {
-			return nil, err
+			return filteredAssets, err
 		}
 
-		var asset Asset
+		var asset LNGDocument
 		err = json.Unmarshal(queryResponse.Value, &asset)
 		if err != nil {
-			return nil, err
+			return filteredAssets, err
 		}
-
-		t := reflect.TypeOf(asset)
-
-		for i := 0; i < t.NumField(); i++ {
-			if t.Field(i).Name == "GPS" {
-				for _, gps := range asset.GPS {
-					if gps.Name == name {
-					assets = append(assets, &asset)
-					break
-					}
-				}
+		if ownerName == asset.Owner {
+			assetInfo := AssetInfoOwner{
+				DocumentID: asset.DocumentID,
+				DocumentCID: asset.DocumentCID,
+				Owner:      asset.Owner,
+				AccessList: asset.AccessList,
+				Timestamp: 		asset.Timestamp,
 			}
+			filteredAssets = append(filteredAssets, assetInfo)
 		}
-
 	}
-
-	return assets, nil
+	return filteredAssets, nil
 }
 
 
+func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface) ([]*LNGDocument, error) {
+    resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+    if err != nil {
+        return nil, fmt.Errorf("failed to get assets: %v", err)
+    }
+    defer resultsIterator.Close()
 
-func hash(newasset Asset) Asset {
+    var assets []*LNGDocument
+    for resultsIterator.HasNext() {
+        queryResponse, err := resultsIterator.Next()
+        if err != nil {
+            return nil, fmt.Errorf("failed to iterate over assets: %v", err)
+        }
+
+        var asset LNGDocument
+        err = json.Unmarshal(queryResponse.Value, &asset)
+        if err != nil {
+            return nil, fmt.Errorf("failed to unmarshal asset: %v", err)
+        }
+
+        assets = append(assets, &asset)
+    }
+
+    return assets, nil
+}
+
+
+func (s *SmartContract) GetQR(ctx contractapi.TransactionContextInterface, value1 string, value2 string) (string, error) {
+	// range query with empty string for startKey and endKey does an
+	// open-ended query of all assets in the chaincode namespace.
+	var asserDoc string
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+		return asserDoc, err
+	}
+	defer resultsIterator.Close()
+
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return asserDoc, err
+		}
+
+		var asset LNGDocument
+		err = json.Unmarshal(queryResponse.Value, &asset)
+		if err != nil {
+			return asserDoc, err
+		}
+
+		if asset.DocumentID == value1 && asset.Owner == value2 {
+			return asset.QR, nil
+		}
+	}
+
+	return "", nil
+
+}
+
+
+func hash(newasset LNGDocument) LNGDocument {
 	hash := sha256.New()
 	structString := fmt.Sprintf("%+v", newasset)
 	_, err := hash.Write([]byte(structString))
@@ -274,45 +284,25 @@ func hash(newasset Asset) Asset {
 
 }
 
-func code(newasset Asset) {
+func accesslist(oldAsset LNGDocument, access string) LNGDocument {
+	oldAsset.AccessList = make([]string, 0)
 
-	err := qrcode.WriteFile(newasset.QR, qrcode.High, 256, fmt.Sprintf("/tme/%s.png", newasset.ID))
-	if err != nil {
-		fmt.Println("Error generating QR code:", err)
+	accessList := strings.Split(access, ", ")
+
+	for _, a := range accessList {
+		oldAsset.AccessList = append(oldAsset.AccessList, a)
 	}
 
-}
-
-func add(newasset Asset, category string, value string) Asset {
-	// Create a map and add some key-value pairs to it
-	data := make(map[string]string)
-	data[category] = value
-	// Create a new struct and add the map to it
-	newasset.Additional = data
-	// Return the struct
-	return newasset
-}
-
-func gps(newasset Asset, latitude float64, longitude float64, namestamp string) Asset {
-	
-	gps := GPSCoordinate{
-        Name:      namestamp,
-        Latitude:  latitude,
-        Longitude: longitude,
-        Timestamp: time.Now().Format("15:04:05 02-01-2006"),
-    }
-	newasset.GPS = append(newasset.GPS, gps)
-
-	return newasset
+	return oldAsset
 }
 
 
-func DecodeAsset(data []byte) (Asset, error) {
-    var asset Asset
+
+func DecodeAsset(data []byte) (LNGDocument, error) {
+    var asset LNGDocument
     err := json.Unmarshal(data, &asset)
     if err != nil {
-        return Asset{}, err
+        return LNGDocument{}, err
     }
     return asset, nil
 }
-
